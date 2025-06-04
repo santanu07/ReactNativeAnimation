8fb747bac1bf3c4038d2e0758c40a35fab9e6b1b
Santanu Hazra <santanu.hazra@stockedge.com>
2025-06-04 18:02:16 +0530

    navigation added

---
+++ src/screens/ProfileScreens.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+function ProfileScreen() {
+  return (
+    <View>
+      <Text>ProfileScreen</Text>
+    </View>
+  );
+}
+export default ProfileScreen;
